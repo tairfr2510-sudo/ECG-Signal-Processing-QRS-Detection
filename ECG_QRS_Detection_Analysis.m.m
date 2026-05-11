e3e9493335907b0f99d1e903644734301ae4af46
tairfr2510-sudo <tairfr2510@gmail.com>
2026-05-11 11:26:44 +0300

    Add files via upload

---
+++ ECG_QRS_Detection_Analysis.m.m
@@ -0,0 +1,31 @@
+clc
+clear
+close all
+
+%% Loading Data and Constants
+% insert your code HERE
+% load('');
+% Fs = ;
+
+%% Q(1)
+figure;
+time = (1:length(ecg1))/Fs;
+% insert your code HERE
+% plot(,);
+hold on;
+% plot(,);
+% xlabel('');
+% ylabel ('');
+legend('Signal', 'QRS detections','Location','southeast');
+
+
+%% Q(2)
+figure()
+peaks_ecg1 = double(peaks_ecg1);
+% insert your code HERE
+
+
+%% Q(3)
+figure;
+% insert you code HERE
+
